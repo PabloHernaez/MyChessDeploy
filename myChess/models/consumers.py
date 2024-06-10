244e1c846b92be63a940440e05719d9f35a46289
YaserJafar <92629580+YaserJafar@users.noreply.github.com>
2024-06-10 18:59:13 +0200

    Update consumers.py

--- myChess/models/consumers.py
+++ myChess/models/consumers.py
@@ -124,7 +124,7 @@ class ChessConsumer(AsyncWebsocketConsumer):
                 message = f'Error: an exception has been produced ' + str(ex)
                 await self.send(text_data=json.dumps({"type":'error', "message" : message}))
         
-        elif (data['type'] == 'move'):
+        elif (data['type'] == 'message'):
             print("Se trata de un mensaje")
             try:
                 playerID = data['playerID']
